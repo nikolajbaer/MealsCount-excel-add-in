1245030cd26dc856f2ca780a70a3050a306a6545
Jim Goddard <44912384+jameswgoddard@users.noreply.github.com>
2021-01-31 22:55:38 +0000

    Initial commit. Pulls JSON from JSONPlaceholder and puts it in A1. From here it should be trivial to parse JSON and put parsed results in the right cells

---
+++ MealsCountWeb/Home.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+(function () {
+
+    Office.onReady(function () {
+        // Office is ready
+        $(document).ready(function () {
+            // The document is ready
+            $('#refresh').click(refreshData);
+        });
+    });
+
+    async function pollEndpoint(url) {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    }
+
+    function refreshData() {
+        Excel.run(function (context) {            
+            data = pollEndpoint('https://jsonplaceholder.typicode.com/todos/1')
+                .then(function (data) {
+                    let sheet = context.workbook.worksheets.getActiveWorksheet();
+                    let range = sheet.getRange("A1");
+                    range.values = JSON.stringify(data);
+                    context.sync();
+                });            
+        }).catch(function (error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+    }
+})();
