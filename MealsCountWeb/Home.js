b36120548312e8fe8387f05e9edfa5ede94731e9
Jim Goddard <44912384+jameswgoddard@users.noreply.github.com>
2021-02-06 16:31:34 +0000

    Checking selected range isn't populated for real this time. This works but feels like a hack

--- MealsCountWeb/Home.js
+++ MealsCountWeb/Home.js
@@ -16,26 +16,20 @@
         return data;
     }
 
-    async function checkRange(rangeStr, url) {
-        let sheet = context.workbook.worksheets.getActiveWorksheet();
-        let range = sheet.getRange(rangeStr);
-        if (range.values.data === undefined) {
-            let data = await pollEndpoint(url);
-            return data;
-        } else {
-            throw 'Suggested range is already populated'
-        }
-    }
-
     function refreshData() {
         Excel.run(function (context) {            
-            data = checkRange('https://jsonplaceholder.typicode.com/todos/1')
+            data = pollEndpoint('https://jsonplaceholder.typicode.com/todos/1')
                 .then(
                     function (data) {
                         let sheet = context.workbook.worksheets.getActiveWorksheet();
                         let range = sheet.getRange("A1");
-                        range.values = JSON.stringify(data);
-                        context.sync();
+                        let text = range.convertDataTypeToText();
+                        if (text === undefined) {
+                            range.values = JSON.stringify(data);
+                            context.sync();
+                        }else {
+                            throw 'Values already present in range'
+                        }
                     },
                     function (err) {
                         console.log(err);
