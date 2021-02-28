135e57345237809617cc7189f69a6c77e890c6ea
Jim Goddard <44912384+jameswgoddard@users.noreply.github.com>
2021-02-28 14:23:41 +0000

    Works within the Add-in when given good input. Need to add some error handling but this at least works

--- MealsCountWeb/Home.js
+++ MealsCountWeb/Home.js
@@ -23,18 +23,14 @@
     }
 })()
 
-    
-
 // React to user's button click
 function refreshData() {
 
     // Compute new values and add them to school info object
     function enrichSchoolInfo(schoolInfo, state) {
-        // Validate input
+        // Validate input?
 
-        // Translate schoolInfo into array that can be used to populate worksheet
-
-        // For testing ignore the input and parse a hardcoded sample
+        // Example
         //schoolInfo = { "name": "Queen Creek Unified District", "code": "70295000", "total_enrolled": 7627, "overall_isp": 0.07657007997902189, "school_count": 9, "best_strategy": null, "est_reimbursement": 0.0, "rates": { "free_lunch": 3.41, "paid_lunch": 0.32, "free_bfast": 1.84, "paid_bfast": 0.31 }, "schools": [{ "school_code": "70295102", "school_name": "Desert Mountain Elementary", "school_type": "n/a", "total_enrolled": 657, "total_eligible": 51, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0776, "active": true }, { "school_code": "70295107", "school_name": "Faith Mather Sossaman Elementary School", "school_type": "n/a", "total_enrolled": 660, "total_eligible": 50, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0758, "active": true }, { "school_code": "70295104", "school_name": "Frances Brandon-Pickett Elementary", "school_type": "n/a", "total_enrolled": 548, "total_eligible": 42, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0766, "active": true }, { "school_code": "70295105", "school_name": "Gateway Polytechnic Academy", "school_type": "n/a", "total_enrolled": 853, "total_eligible": 56, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0657, "active": true }, { "school_code": "70295103", "school_name": "Jack Barnes Elementary School", "school_type": "n/a", "total_enrolled": 377, "total_eligible": 31, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0822, "active": true }, { "school_code": "70295121", "school_name": "Newell Barney Middle School", "school_type": "n/a", "total_enrolled": 877, "total_eligible": 67, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0764, "active": true }, { "school_code": "70295101", "school_name": "Queen Creek Elementary School", "school_type": "n/a", "total_enrolled": 620, "total_eligible": 65, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.1048, "active": true }, { "school_code": "70295201", "school_name": "Queen Creek High School", "school_type": "n/a", "total_enrolled": 2158, "total_eligible": 166, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0769, "active": true }, { "school_code": "70295106", "school_name": "Queen Creek Middle School", "school_type": "n/a", "total_enrolled": 877, "total_eligible": 56, "daily_breakfast_served": null, "daily_lunch_served": null, "isp": 0.0639, "active": true }] };
         schoolInfo['state_code'] = state;
         for (let school of schoolInfo.schools) {
@@ -107,47 +103,26 @@ function refreshData() {
 
     // Fetch data and populate newly created worksheet
 
-    /*
+    
     // Doesn't work
     fetch(url)
         .then(resp => resp.json())
         .then(data => enrichSchoolInfo(data, state))
-        .then(enrichedData => {
-            optimize(enrichedData)
-                .then(optimalSolution => {
-                    Excel.run(context => {
-                        // Get Excel objects
-                        let sheet = context.workbook.worksheets.getItem("Meals Count");
-                        let range = sheet.getRange("A1");
-                        let range2 = sheet.getRange("A2");
-
-                        // Define what data should go in proxy objects
-                        range.values = formatOptimalSolution(optimalSolution);
-                        range2.values = "Hello World";
-
-                        // Actually put that data there
-                        return context.sync().then(() => console.log('Populated workbook'))
-                    })
-                }).catch(err => console.error(err));
-        }).catch(err => console.error(err));
-        */
-    // Works
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => enrichSchoolInfo(data, state))
-        .then(enrichedData => {
+        .then(async function (enrichedData) {
+            let optimalSolution = await optimize(enrichedData);
             Excel.run(context => {
                 // Get Excel objects
                 let sheet = context.workbook.worksheets.getItem("Meals Count");
                 let range = sheet.getRange("A1");
-                let range2 = sheet.getRange("A2");
 
                 // Define what data should go in proxy objects
-                range.values = formatOptimalSolution(enrichedData);
-                range2.values = "Hello World";
+                range.values = optimalSolution;
 
                 // Actually put that data there
-                return context.sync().then(() => console.log('Populated workbook'))
-            }).catch(err => console.error(err));
+                return context.sync().then(() => {
+                    working.style.display = "none";
+                    console.log('Populated workbook');
+                });
+            })
         }).catch(err => console.error(err));
 }
